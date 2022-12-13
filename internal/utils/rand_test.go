a4e6c5032a3b2cce1864e436e890ee8433673e4e
Vladislav Bag <evgeng49@yandex.ru>
2022-12-13 17:04:45 +0300

    add test for random bytes

---
+++ internal/utils/rand_test.go
@@ -0,0 +1,32 @@
+package utils
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestGenerateRandomBytes(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    []byte
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := GenerateRandomBytes(tt.args.n)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GenerateRandomBytes() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GenerateRandomBytes() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
